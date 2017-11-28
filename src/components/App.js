a7cb70df18dbdb2de98d19b08f26dc8f8accce53
Ramiro Pedrozo <ramiro.pedrozo@gmail.com>
2017-11-28 17:25:21 -0200

    feat:  add connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react'
-import { addRecipe } from '../actions'
-
-class App extends Component {
-  
-  render() {
-    return (
-      <div>
-        hello
-      </div>
-    )
+  import React, { Component } from 'react'
+  import { addRecipe } from '../actions'
+  import {connect} from 'react-redux'
+  class App extends Component {
+    
+    render() {
+      console.log('PROPS',this.props);
+      return (
+        <div>
+          hello
+        </div>
+      )
+    }
   }
-}
 
-export default App
+  function matStateToProps(calendar){
+    const daysOrder = ['sunday','monday','tuesday','wednesday','thursday','friday']
+    return{
+        calendar : daysOrder.map((day)=>({
+          day,
+          meals: Object.keys(calendar[day]).reduce((meals,meal)=>{
+            meals[meal] = calendar[day][meal] ? calendar[day][meal] : null
+            return meals;
+          },{})
+        }))
+    };
+  }
+
+  export default connect(matStateToProps)(App)
